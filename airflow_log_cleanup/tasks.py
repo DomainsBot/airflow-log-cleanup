6ca5fca53748a84cfbd23c3a0a7b1d274833e2e1
Javier Ruere <javier@ruere.com.ar>
2023-10-25 19:58:22 +0200

    Fix date comparison.

--- airflow_log_cleanup/tasks.py
+++ airflow_log_cleanup/tasks.py
@@ -6,7 +6,7 @@ import pendulum
 
 
 def cleanup_before_date(base_folder: pathlib.Path | str, pattern: str, days_ago: int) -> None:
-    before_date = pendulum.today(tz="UTC").subtract(days=days_ago)
+    before_date = pendulum.today(tz="UTC").subtract(days=days_ago).date()
     paths = pathlib.Path(base_folder).rglob('*')
     dirs = (path for path in paths if path.is_dir())
     for dir_ in dirs:
